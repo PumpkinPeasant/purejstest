b0ac5fc15e9c04e19871e065c5b8f793e6f550d5
PumpkinPeasant <m2314670@edu.misis.ru>
2025-03-01 12:50:36 +0300

    Data table

--- src/main.js
+++ src/main.js
@@ -16,20 +16,36 @@ class Person {
 
 }
 
-document.querySelector('#app').innerHTML = `
-<h1>Hello, !</h1>
-<button id="button" type="button">Say Hi!</button>
-`
+// document.querySelector('#app').innerHTML = `
+// <h1>Hello, !</h1>
+// <button id="button" type="button">Say Hi!</button>
+// `
+//
+// document.querySelector('#button').onclick = () => user.sayHi();
 
-document.querySelector('#button').onclick = () => user.sayHi();
-
-let people = null
+let people = []
 
 async function fetchPeople() {
     const response = await fakeApi();
-    return response.map(person => new Person(person.name, person.gender, person.salary, person.rating));
+    people = response.map(person => new Person(person.name, person.gender, person.salary, person.rating));
+    renderTable(people);
 }
 
-fetchPeople().then((peopleData) => {
-    console.log(peopleData); // Теперь точно будет массив Person
-});
+fetchPeople();
+
+
+function renderTable(people) {
+    const tableBody = document.querySelector("#employeeTable tbody");
+
+    people.forEach(person => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${person.name}</td>
+            <td>${person.gender}</td>
+            <td>${person.salary}</td>
+            <td>${person.rating}</td>
+            <td><button class="button-accent">Hire</button></td>
+        `;
+        tableBody.appendChild(row);
+    })
+}
